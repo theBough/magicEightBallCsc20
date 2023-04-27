b8797c42b0f299a149fd4e8e9e54bf65954e8d68
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-04-27 10:08:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var myBtn = document.getElementById("myBtn");
+var theirQuestion = document.getElementById("theirQuestion")
+var answer = document.getElementById("answer")
 
 function test(){
   myBtn.innerHTML = "Hello World"
+  answer.innerHTML += "<br>You:>>" + theirQuestion.value;
 }
